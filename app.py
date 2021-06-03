f840631cdeb8b6a1fbfdf010d235fda1f0e3e52b
fearlee <firlimoo1@gmail.com>
2021-06-03 14:14:15 +0700

    v1

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import json
+import load_model
+import os
+
+app = Flask(__name__)
+@app.route("/", methods=["GET"])
+def jawab_get():
+    jawaban={"message":"JSON post request tidak ada"}
+    return json.dumps(jawaban)
+
+@app.route("/", methods=['POST'])
+def jawab_api():
+    #request_json = request.get_json()
+    if request.json["message"] is not None:
+        kalimat = request.json["message"]
+        hasil_predict = load_model.load(kalimat)
+        jawaban = {
+            "request" : request.json["message"],
+            "message" : hasil_predict
+        }
+    
+    else:
+        jawaban = {
+            "Error" : "NoneType object JSON request",
+            "message" : request.json["message"]
+        }
+    
+    return json.dumps(jawaban)
+
+
+if __name__ == '__main__':
+    # app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+# for testing purpose
+#print(load_model.load("ini kaliat test yang ndak terlalu panjang"))
